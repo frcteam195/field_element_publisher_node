9fc2a4dc17690270ea24b1b12f2fa9d4ebf97ea7
Michael Todd <eltodd@gmail.com>
2022-01-22 03:11:03 +0000

    Updates

--- src/field_element_publisher_node.cpp
+++ src/field_element_publisher_node.cpp
@@ -5,7 +5,42 @@
 #include <string>
 #include <mutex>
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/TransformStamped.h>
+#pragma GCC diagnostic pop
+
 ros::NodeHandle* node;
+
+tf2_ros::TransformBroadcaster * tfBroadcaster;
+
+
+void publisher_loop(void)
+{
+    while(ros::ok())
+    {
+		geometry_msgs::TransformStamped transformStamped;
+
+		transformStamped.header.stamp = ros::Time::now();
+		transformStamped.header.frame_id = "base_link";
+		transformStamped.child_frame_id = "test_state";
+
+		transformStamped.transform.translation.x = -5;
+		transformStamped.transform.translation.y = 0;
+		transformStamped.transform.translation.z = 0;
+
+		tf2::Quaternion q;
+		q.setRPY(0,0,0);
+		transformStamped.transform.rotation.x = q.x();
+		transformStamped.transform.rotation.x = q.y();
+		transformStamped.transform.rotation.x = q.z();
+		transformStamped.transform.rotation.x = q.w();
+
+		tfBroadcaster->sendTransform(transformStamped);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -24,6 +59,10 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 
 	node = &n;
+
+    tfBroadcaster = new tf2_ros::TransformBroadcaster();
+
+    std::thread publisher_thread(publisher_loop);
 
 	ros::spin();
 	return 0;
